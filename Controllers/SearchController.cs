1b0b722442086f808b15e21719ba57550ebf5ea4
hencccc <henric1@hotmail.se>
2022-10-12 11:55:03 +0200

    search is now done

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using databaskonstruktion.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace databaskonstruktion.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private FoodModel _foodModel;
+
+        public SearchController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _foodModel = new FoodModel(_configuration);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SearchFood(int magic)
+        {
+            ViewBag.SearchFood = _foodModel.SearchFood(magic);
+            return View();
+        }
+    }
+}
--- Models/FoodModel.cs
+++ Models/FoodModel.cs
@@ -26,5 +26,19 @@ namespace databaskonstruktion.Models
 
             return foodTable;
         }
+
+        public DataTable SearchFood(int magic)
+        {
+            MySqlConnection dbcon = new MySqlConnection(_connectionString);
+            dbcon.Open(); 
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM Mat where Maginivå=@MAGIC;", dbcon);
+            adapter.SelectCommand.Parameters.AddWithValue("@MAGIC", magic);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable foodTable = ds.Tables["result"];
+            dbcon.Close();
+
+            return foodTable;
+        }
     }
 }
